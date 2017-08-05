afea34493ce68f7bfd07c2471442c035e333efc0
Declan Caveney <deccav@users.noreply.github.com>
2017-08-05 14:13:28 +0100

    Add files via upload

---
+++ debian-setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# A script designed to make post-install setup easier for Debian based distributions.
+
+echo "Choices: Chrome (1), Spotify (2), Gitkraken (3), Steam (4), VLC (5), GIMP (6)"
+echo ""
+echo "Please press the corresponding number for every\nprogram you'd like to install, then press enter."
+
+echo ""
+echo "Choices: "
+read choices
+
+echo ""
+echo "Confirm that you would like to proceed (y/n): "
+read continue
+if [ $continue = "y" ];
+then
+
+# Update sources
+apt-get update
+
+# Install Gdebi
+apt-get -y install gdebi-core
+
+# Download Google Chrome and install
+if [[ $choices == *"1"* ]]; then
+	cd /tmp &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_curr‌​ent_amd64.deb &&
+	gdebi google-chrome-stable_current_amd64.deb
+fi
+
+# Add Spotify keys, repository, then download and install
+if [[ $choices == *"2"* ]]; then
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410 &&
+	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list &&
+	apt-get update &&
+	apt-get -y install spotify-client
+fi
+
+# Download and install Gitkraken
+if [[ $choices == *"3"* ]]; then
+	cd /tmp &&
+	wget https://release.gitkraken.com/linux/gitkraken-amd64.deb &&
+	gdebi gitkraken-amd64.deb
+fi
+
+# Download and install Steam
+if [[ $choices == *"4"* ]]; then
+	cd /tmp &&
+	wget http://media.steampowered.com/client/installer/steam.deb &&
+	gdebi steam.deb
+fi
+
+# Download and install VLC
+if [[ $choices == *"5"* ]]; then
+	apt-get -y install vlc
+fi
+
+# Download and install GIMP
+if [[ $choices == *"6"* ]]; then
+	apt-get -y install gimp
+fi
+fi
+
+echo ""
+echo "Script complete - exiting"
+exit
